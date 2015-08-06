a944d63e4a16cda1c80db9daa16909495ac84b14
Lucas Toledo <ltoledo@wkm.com.br>
2015-08-06 14:37:00 -0300

    adicionado componentes de input_masked

--- dist/js/wkm-react-frontend.js
+++ dist/js/wkm-react-frontend.js
@@ -1859,6 +1859,101 @@ var InputHidden = React.createClass({displayName: "InputHidden",
   }
 });
 
+var InputMasked = React.createClass({displayName: "InputMasked",
+  mixins: [CssClassMixin, InputComponentMixin],
+
+  propTypes: {
+    mask: React.PropTypes.string,
+    clearmaskonlostfocus: React.PropTypes.boolean,
+    clearIncomplete: React.PropTypes.boolean,
+    showMaskOnHover: React.PropTypes.boolean,
+    showMaskOnFocus: React.PropTypes.boolean,
+    isRegex: React.PropTypes.boolean,
+    predefinedMasks: React.PropTypes.object
+  },
+
+  getDefaultProps: function() {
+    return {
+      themeClassKey: 'input.text',
+      mask:'',
+      isRegex: false,
+      clearmaskonlostfocus: true,
+      clearIncomplete:false,
+      showMaskOnHover: false,
+      showMaskOnFocus: false,
+      predefinedMasks: {
+        cpf: '999.999.999-99',
+        cnpj: '99.999.999/9999-99',
+        phone: '(99) 9999[9]-9999',
+        cell_phone: '(99) 9999[9]-9999',
+        date: '99/99/9999',
+        date_time: '99/99/9999 [99:99:99]'
+      }
+    };
+  },
+
+  render: function() {
+    return (
+        React.createElement("input", React.__spread({},  this.props, {className: this.className(), ref: "inputMasked", type: "text"}))
+    );
+  },
+
+  componentDidMount: function(){
+    if(this.props.isRegex)
+      this.renderRegexMask();
+    if(this.isAPredefinedMask(this.props.mask))
+      this.renderPredefinedMask();
+    else
+      this.renderCustomMask();
+  },
+
+  renderRegexMask: function(){
+    var params = this.getDefaultConfigPlugin();
+    params['regex'] = this.props.mask;
+
+    this.renderBaseMask('Regex',params)
+  },
+  renderCustomMask: function(){
+    var params = this.getDefaultConfigPlugin();
+    params['mask'] = this.props.mask;
+
+    this.renderBaseMask(params)
+  },
+  renderPredefinedMask: function(){
+    var params = this.getDefaultConfigPlugin();
+    params['mask'] = this.maskMapping(this.props.mask);
+
+    this.renderBaseMask(params)
+  },
+
+  renderBaseMask: function(type,params){
+    if(type != undefined)
+      $(React.findDOMNode(this.refs.inputMasked)).inputmask(type,params);
+    else
+      $(React.findDOMNode(this.refs.inputMasked)).inputmask(params);
+  },
+
+  //Functions Utils
+  getDefaultConfigPlugin : function(){
+    return {
+      clearIncomplete: this.props.clearIncomplete,
+      showMaskOnHover: this.props.showMaskOnHover,
+      showMaskOnFocus: this.props.showMaskOnFocus
+    }
+  },
+
+  maskMapping: function(type) {
+    var maskTypes = this.props.predefinedMasks;
+    return maskTypes[type] == undefined ? type : maskTypes[type];
+  },
+
+  isAPredefinedMask: function(value){
+    return value in this.props.predefinedMasks
+  }
+
+});
+
+
 var InputPassword = React.createClass({displayName: "InputPassword",
   mixins: [CssClassMixin, InputComponentMixin],
   propTypes: {
