14603bdf4e90dbf2f9215c7dea6da9551afed56a
Lucas Toledo <ltoledo@wkm.com.br>
2015-08-14 14:01:26 -0300

    [WKMRF-26] - In-Progress - Refactor reaproveitando o Mixin do SelectComponentMixin.

--- dist/js/wkm-react-frontend.js
+++ dist/js/wkm-react-frontend.js
@@ -2871,50 +2871,24 @@ var InputRadio = React.createClass({displayName: "InputRadio",
 });
 
 var InputRadioGroup = React.createClass({displayName: "InputRadioGroup",
-  mixins: [CssClassMixin, InputComponentMixin],
+  mixins: [
+    CssClassMixin,
+    InputComponentMixin,
+    SelectComponentMixin
+  ],
+
   propTypes: {
     name: React.PropTypes.string,
     align: React.PropTypes.oneOf(['vertical', 'horizontal']),
-    options: React.PropTypes.array,
-    currentValue: React.PropTypes.string,
-    url:React.PropTypes.string,
-    url_key:React.PropTypes.string,
-    url_value:React.PropTypes.string
+    currentValue: React.PropTypes.string
   },
 
   getDefaultProps: function() {
     return {
       name:'',
       align: 'vertical',
-      options: null,
-      currentValue: null,
-      url:null,
-      url_key:'id',
-      url_value:'name'
+      currentValue: null
     };
-  },
-
-  getOptionsFromUrl: function(){
-    var result=[];
-    var context = this;
-    $.ajax({
-      type: 'GET',
-      async: false,
-      url: 'http://jsonstub.com/cities',
-      contentType: 'application/json',
-      beforeSend: function (request) {
-        request.setRequestHeader('JsonStub-User-Key', '2e13daef-b39e-44c9-8159-af8c53d7f7f5');
-        request.setRequestHeader('JsonStub-Project-Key', '46772881-8bbc-46e8-9c16-5062f73138a4');
-      }
-    }).done(function (data) {
-      $.each(data,function(i,item){
-        result.push({
-          'value': item[context.props.url_key],
-          'name': item[context.props.url_value]
-        });
-        context.props.options = result;
-      });
-    });
   },
 
   getInitialState: function() {
@@ -2923,43 +2897,34 @@ var InputRadioGroup = React.createClass({displayName: "InputRadioGroup",
     }
   },
 
-  renderItems: function() {
-    (this.props.url != '')? this.getOptionsFromUrl() : this.props.options;
+  renderOptions: function() {
+    var selectOptions = [];
+    var options = this.state.options;
 
-    return this.props.options.map(function (item, i) {
+    for(var i = 0; i < options.length; i++) {
+      var optionProps = options[i];
+      optionProps['id'] = this.props.name + '_' + i,
+      optionProps['type'] = 'radio';
 
-      var optionsInput = {
-        type:'radio',
-        id: this.props.name + '_' + i,
-        name: this.props.name,
-        value: item.value
-      };
-      if (this.state.currentValue === item.value)
-        optionsInput['defaultChecked'] = "checked";
+      if (this.state.currentValue === optionProps.value)
+        optionProps['defaultChecked'] = optionProps.value;
 
-      return (
+      selectOptions.push(
         React.createElement("p", null, 
-          React.createElement("input", React.__spread({},  optionsInput )), 
-          React.createElement(Label, {id: optionsInput.id, label: item.name})
+          React.createElement("input", React.__spread({},  optionProps , {name: this.props.name})), 
+          React.createElement(Label, {id: optionProps.id, label: optionProps.name})
         )
       );
-    }, this);
+    }
+    return selectOptions;
   },
 
   render: function() {
     return (
       React.createElement("div", {className: 'input-checkbox-group align-' + this.props.align, ref: "radioGroup"}, 
-        this.renderItems()
+        this.renderOptions()
       )
     );
-  },
-
-  handleClick: function(event) {
-    $label = $(event.currentTarget);
-    $radio = $(React.findDOMNode(this.refs.radioGroup)).find("#"+$label.attr('for'));
-
-    this.setState({currentValue: $radio.val()});
-
   }
 
 });
