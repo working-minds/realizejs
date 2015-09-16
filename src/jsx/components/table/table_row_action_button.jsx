87eaecb4bda80465edcdfc4f092109872a80dac2
Lucas Toledo <ltoledo@wkm.com.br>
2015-09-16 11:38:41 -0300

    WKMRF-88 [Resolved] - Incluido componente de TableRowActionButton.

---
+++ src/jsx/components/table/table_row_action_button.jsx
@@ -0,0 +1,100 @@
+var TableRowActionButton = React.createClass({
+  mixins: [CssClassMixin, RequestHandlerMixin],
+
+  propTypes: {
+    data: React.PropTypes.object,
+    selectedRowIds: React.PropTypes.array,
+    selectedRowIdsParam: React.PropTypes.string,
+    allSelected: React.PropTypes.bool,
+    allSelectedData: React.PropTypes.object,
+    count: React.PropTypes.number,
+    actionUrl: React.PropTypes.string,
+    method: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    conditionToShowActionButton: React.PropTypes.func,
+    component: React.PropTypes.string,
+    element: React.PropTypes.string
+  },
+
+  getDefaultProps: function() {
+    return {
+      selectedRowIds: [],
+      allSelected: false,
+      method: null,
+      conditionParams: null,
+      disabled: false,
+      component: null,
+      element: 'a',
+      themeClassKey: 'button.flat',
+      conditionToShowActionButton: function(data) { return true }
+    };
+  },
+
+  render: function() {
+    return (
+        <span>
+        {this.renderButton()}
+        </span>
+    );
+  },
+
+  renderButton: function(){
+    var component = [];
+    if (this.props.conditionToShowActionButton(this.props.conditionParams))
+      if(!!this.props.component){
+        return React.createElement(eval(this.props.component), this.props)
+      } else {
+        component.push(
+          <Button {...this.props}
+            method={this.actionButtonMethod(this.props)}
+            href={this.actionButtonHref(this.props)}
+            onClick={this.actionButtonClick.bind(this, this.props)}
+          />
+        );
+      }
+
+    return component;
+  },
+
+  actionButtonMethod: function(actionButtonProps) {
+    var buttonHref = actionButtonProps.href;
+    if(!buttonHref) {
+      return null;
+    }
+
+    return actionButtonProps.method;
+  },
+
+  actionButtonHref: function(actionButtonProps) {
+    var buttonHref = actionButtonProps.href;
+    if(!!buttonHref) {
+      var dataRowId = this.props.data[this.props.dataRowIdField];
+      buttonHref = buttonHref.replace(/:id/, dataRowId);
+    }
+
+    return buttonHref;
+  },
+
+  actionButtonClick: function(actionButtonProps, event) {
+    var buttonOnClick = actionButtonProps.onClick;
+    var buttonAction = actionButtonProps.actionUrl;
+
+    if($.isFunction(buttonOnClick)) {
+      var dataRowId = this.props.dataRowIdField;
+      buttonOnClick(event, dataRowId, this.props.data);
+    } else if(!!buttonAction) {
+      var actionData = this.getActionData(actionButtonProps);
+      this.performRequest(buttonAction, actionData, (this.props.method || 'POST'));
+    }
+  },
+
+  getActionData: function(actionButtonProps) {
+    var dataIdParam = actionButtonProps.dataIdParam || 'id';
+    var dataRowId = this.props.dataRowIdField;
+    var actionData = {};
+
+    actionData[dataIdParam] = dataRowId;
+    return actionData;
+  }
+
+});
