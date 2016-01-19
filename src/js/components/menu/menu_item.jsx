559ad0f3b15bd2438773923c99a63f82ad776bf6
Rafael Ferrari <rferrari@wkm.com.br>
2016-01-19 11:30:45 -0200

    [RLZUI-113] Resolved - Corrigido validação dos elementos-filho do componente Menu para MenuItem. - Movido subcomponente MenuItem para um arquivo separado. - Iniciado depreciação da prop 'ref_id' do componente Menu.

---
+++ src/js/components/menu/menu_item.jsx
@@ -0,0 +1,28 @@
+var MenuItem = React.createClass({
+  propTypes: {
+    icon: React.PropTypes.string,
+    iconAlign: React.PropTypes.string,
+    href: React.PropTypes.string,
+    target: React.PropTypes.string,
+    onClick: React.PropTypes.object,
+    className: React.PropTypes.string,
+    method: React.PropTypes.string,
+    element: React.PropTypes.string
+  },
+
+  getDefaultProps: function() {
+    return {
+      iconAlign: 'left',
+      method: 'get',
+      element: 'a'
+    };
+  },
+
+  render: function() {
+    return (
+      <li>
+        <Button {...this.props} clearTheme={true} name={this.props.text} />
+      </li>
+    );
+  }
+});
