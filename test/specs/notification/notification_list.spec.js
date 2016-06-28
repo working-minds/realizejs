1d62b8e757ec15d1e506c0572c5ddcf5bb4368d5
lsantos <lsantos@wkm.com.br>
2016-06-28 15:17:55 -0300

    [RLZUI-181]   - Novos testes.

---
+++ test/specs/notification/notification_list.spec.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import $ from 'jquery'
+import ReactDOM from 'react-dom'
+import Realize from 'realize'
+import {NotificationsList} from 'components/notification'
+import {assert} from 'chai';
+import {shallow} from 'enzyme';
+
+describe('NotificationsList', () => {
+  it('exists', () => {
+    assert.ok(NotificationsList);
+  });
+  it('renders with the default props', () => {
+    const content = shallow(
+      <NotificationsList />
+    );
+    assert.ok(content.find('NotificationsList'));
+  });
+});
---
+++ test/specs/sidenav/sidenav.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import $ from 'jquery'
+import ReactDOM from 'react-dom'
+import Realize from 'realize'
+import {SideNav} from 'components/sidenav'
+import {Menu} from 'components/menu'
+import {assert} from 'chai';
+import {shallow, mount} from 'enzyme';
+
+describe('Sidenav', () => {
+  it('exists', () => {
+    assert.ok(SideNav);
+  });
+  it('renders with the default props', () => {
+    const content = shallow(
+      <SideNav />
+    );
+    assert.ok(content.find('SideNav'));
+  });
+  it('renders with Menu', () => {
+    const content = shallow(
+      <SideNav>
+         <Menu ref_id='1'>
+         </Menu>
+     </SideNav>
+    );
+    assert.equal(content.find('Menu').get(1).props.ref_id, "1");
+  });
+
+});
