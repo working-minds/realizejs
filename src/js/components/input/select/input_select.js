77e75b115c0c538fbed1d053b1b937251c7df85e
lsantos <lsantos@wkm.com.br>
2016-07-20 15:14:46 -0300

    Alteração da InputSelect teste

--- src/js/components/input/select/input_select.js
+++ src/js/components/input/select/input_select.js
@@ -56,7 +56,6 @@ export default class InputSelect extends Component {
     let value = this.state.value;
     Console.log(value)
     if (!this.props.multiple) {
-      Console.log(value)
       value = value[0];
     }
 
