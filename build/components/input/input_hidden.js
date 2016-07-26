72caf8be82fbb5983367c3daaedb464085564ebb
lsantos <lsantos@wkm.com.br>
2016-07-26 14:46:44 -0300

    Alteração do import no input_hidden.

--- build/components/input/input_hidden.js
+++ build/components/input/input_hidden.js
@@ -9,8 +9,6 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _dec, _class;
-
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -18,6 +16,10 @@ var _react2 = _interopRequireDefault(_react);
 var _decorators = require('../../utils/decorators');
 
 var _mixins = require('../../mixins');
+
+var _input_base = require('./input_base');
+
+var _input_base2 = _interopRequireDefault(_input_base);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -27,8 +29,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var InputHidden = (_dec = (0, _decorators.mixin)(InputBase), _dec(_class = function (_Component) {
-  _inherits(InputHidden, _Component);
+var InputHidden = function (_InputBase) {
+  _inherits(InputHidden, _InputBase);
 
   function InputHidden() {
     _classCallCheck(this, InputHidden);
@@ -44,5 +46,6 @@ var InputHidden = (_dec = (0, _decorators.mixin)(InputBase), _dec(_class = funct
   }]);
 
   return InputHidden;
-}(_react.Component)) || _class);
+}(_input_base2.default);
+
 exports.default = InputHidden;
--- src/js/components/input/input_hidden.js
+++ src/js/components/input/input_hidden.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import { mixin } from '../../utils/decorators';
 
 import { InputDatefilterBody } from '../../mixins';
-
-@mixin(InputBase)
-export default class InputHidden extends Component {
+import InputBase from './input_base';
+export default class InputHidden extends InputBase {
   render() {
     return (
       <input {...this.props} type="hidden" ref="input" />
