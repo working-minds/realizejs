b5d585697066a287c844f542b5d67007669a309a
Pedro Jesus <pjesus@wkm.com.br>
2017-08-22 17:10:40 -0300

    Refactor do RequestHandlerMixin: aplicado a utilização do httpClient no lugar do $.ajax.

--- src/js/mixins/request_handler_mixin.js
+++ src/js/mixins/request_handler_mixin.js
@@ -1,105 +1,106 @@
 import PropTypes from '../prop_types';
-import $ from 'jquery';
+import { config } from '../realize';
 
 export default {
   propTypes: {
     onRequest: PropTypes.func,
     onSuccess: PropTypes.func,
     onError: PropTypes.func,
-    onComplete: PropTypes.func
+    onComplete: PropTypes.func,
   },
 
   current_xhr: null,
 
-  getDefaultProps: function() {
+  getDefaultProps() {
     return {
-      onRequest: function(requestData, url) {},
-      onSuccess: function(responseData, status, xhr) { return true; },
-      onError: function(xhr, status, error) { return true; },
-      onComplete: function(xhr, status) { return true; }
+      onRequest() {},
+      onSuccess() { return true; },
+      onError() { return true; },
+      onComplete() { return true; },
     };
   },
 
-  getInitialState: function() {
+  getInitialState() {
     return {
-      isLoading: false
+      isLoading: false,
     };
   },
 
-  performRequest: function(url, requestData, method, dataType) {
-    var requestOptions = {
-      url: url,
+  performRequest(url, requestData, method, dataType) {
+    const requestOptions = {
       data: (requestData || {}),
       method: (method || 'GET'),
-      success: (...args) => this.successCallback(...args),
-      error: (...args) => this.errorCallback(...args),
-      complete: (...args) => this.completeCallback(...args)
     };
 
-    if(!!dataType) {
+    if (!!dataType) {
       requestOptions.dataType = dataType;
     }
 
     this.cancelPendingRequest();
     this.requestCallback(requestData, url);
-    this.current_xhr = $.ajax(requestOptions);
+    this.current_xhr = config.httpClient(url, requestOptions)
+      .then((...args) => this.successCallback(...args))
+      .catch((...args) => this.errorCallback(...args));
   },
 
-  cancelPendingRequest: function() {
-    if(this.current_xhr !== null && this.current_xhr.readyState < 4) {
+  cancelPendingRequest() {
+    if (this.current_xhr !== undefined &&
+        this.current_xhr !== null &&
+        this.current_xhr.readyState < 4) {
+
       this.current_xhr.abort();
     }
   },
 
-  requestCallback: function(requestData, url) {
-    this.setState({isLoading: true});
+  requestCallback(requestData, url) {
+    this.setState({ isLoading: true });
     this.executeCallback('onRequest', requestData, url);
   },
 
-  successCallback: function(responseData, status, xhr) {
-    if(this.executeCallback('onSuccess', responseData, status, xhr)) {
+  successCallback(responseData, status, xhr) {
+    this.setState({ isLoading: false });
+    if (this.executeCallback('onSuccess', responseData, status, xhr)) {
       this.handleSuccess(responseData, status, xhr);
     }
   },
 
-  errorCallback: function(xhr, status, error) {
-    if(error !== "abort") {
+  errorCallback(xhr, status, error) {
+    this.setState({ isLoading: false });
+    if (error !== 'abort') {
       this.executeCallback('onError', xhr, status, error);
     }
   },
 
-  completeCallback: function(xhr, status) {
-    this.setState({isLoading: false});
+  completeCallback(xhr, status) {
+    this.setState({ isLoading: false });
     this.executeCallback('onComplete', xhr, status);
   },
 
-  executeCallback: function(callbackFunction) {
+  executeCallback(callbackFunction) {
     var callbackArguments = Array.prototype.slice.call(arguments, 1);
     var componentFunction = this[callbackFunction];
     var propFunction = this.props[callbackFunction];
 
-    if(typeof componentFunction === "function") {
+    if (typeof componentFunction === 'function') {
       componentFunction.apply(this, callbackArguments);
     } else {
       propFunction.apply(this, callbackArguments);
     }
   },
 
-  handleSuccess: function(responseData, status, xhr) {
+  handleSuccess(responseData, status, xhr) {
     var contentType = xhr.getResponseHeader('Content-Type');
 
-    if(contentType.match(/text\/javascript/)) {
+    if (contentType.match(/text\/javascript/)) {
       this.handleJsResponse(responseData);
-    } else if(contentType.match(/text\/html/)) {
+    } else if (contentType.match(/text\/html/)) {
       this.handleHtmlResponse(responseData);
     }
   },
 
-  handleJsResponse: function(responseJs) {
+  handleJsResponse(responseJs) {
     eval(responseJs);
   },
 
-  handleHtmlResponse: function(responseHtml) {
-
-  }
-}
+  handleHtmlResponse(responseHtml) {},
+};
