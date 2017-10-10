05d6ee4771ca09bc101dbfee7b9ef8e217c717f9
Rafael Ferrari <rferrari@wkm.com.br>
2017-10-10 11:10:30 -0300

    Corrigido bug em validação do Form sem InputGroup

--- test/specs/form/form.spec.js
+++ test/specs/form/form.spec.js
@@ -1,51 +1,85 @@
 import React from 'react'
 import $ from 'jquery'
 import Realize from 'realize'
-import { Form } from 'components/form'
-import { assert } from 'chai';
-import { shallow } from 'enzyme';
+import Form from 'components/form/form';
+import InputGroup from 'components/form/input_group';
+import Input from 'components/input/input';
+import {assert} from 'chai';
+import {shallow, mount} from 'enzyme';
+import Chance from 'chance';
+
+const chance = new Chance();
 
 describe('<Form/>', () => {
-  it('exists', () => {
-    assert(Form);
+  it('renders a form', () => {
+    const content = shallow(<Form />);
+    expect(content.find('form').exists()).to.be.true;
   });
-  it('renders with the default props', () => {
-    const content = shallow(
-      <Form />
-    );
-    assert(content.find('Form'));
+
+  describe('validation', function() {
+    beforeEach(function() {
+      this.sandbox = sinon.sandbox.create();
+
+      this.inputName = chance.md5();
+      this.inputErrors = [chance.md5()];
+      this.postData = { [this.inputName]: chance.md5() };
+      this.validationErrors = { [this.inputName]: this.inputErrors };
+
+      this.validationCallback = sinon.spy();
+      this.submitCallback = sinon.spy();
+
+      this.event = {
+        nativeEvent: { preventDefault: sinon.spy() },
+        preventDefault: sinon.spy(),
+        persist: sinon.spy(),
+      };
+    });
+
+    afterEach(function() {
+      this.sandbox.restore();
+    });
+
+    it('applies validation when InputGroup is mounted', function() {
+      this.sandbox
+        .stub(InputGroup.prototype, 'validate')
+        .returns(this.validationErrors);
+
+      this.sandbox.stub(Form.prototype, 'submit');
+      this.sandbox.stub(Form.prototype, 'serialize').returns(this.postData);
+
+      const content = mount(
+        <Form
+          onValidationErrors={this.validationCallback}
+          onSubmit={this.submitCallback}
+          inputs={{ [this.inputName]: { component: 'text' } }}
+        />
+      );
+
+      content.find('form').simulate('submit', this.event);
+
+      expect(this.submitCallback).to.have.been.called;
+      expect(this.event.preventDefault).to.have.been.called;
+      expect(this.validationCallback).to.have.been.calledWith(this.validationErrors, this.postData);
+    });
+
+    it('ignores validation when InputGroup is not mounted', function() {
+      this.sandbox.stub(Form.prototype, 'submit');
+      this.sandbox.stub(Form.prototype, 'serialize').returns(this.postData);
+
+      const content = mount(
+        <Form
+          onValidationErrors={this.validationCallback}
+          onSubmit={this.submitCallback}
+        >
+          <Input component="text" name={this.inputName} />
+        </Form>
+      );
+
+      content.find('form').simulate('submit', this.event);
+
+      expect(this.submitCallback).to.have.been.called;
+      expect(this.validationCallback).to.not.have.been.called;
+    });
   });
-  it('renders with isLoading', () => {
-    const content = shallow(
-      <Form
-            isLoading
-      />
-    );
-    assert(content.find('isLoading'));
-  });
-  it('renders with otherButtons', () => {
-    const content = shallow(
-      <Form
-           otherButtons= {[
-               {
-                   name: 'actions.clear',
-                   icon: 'delete',
-                   element: 'button',
-                   type: 'reset'
-               }
-           ]}
-       />
-    );
-    assert(content.find('otherButtons'));
-  });
-  it('renders with onSubmit', () => {
-    const content = shallow(
-      <Form
-        onSubmit={function(){
-         alert('Product Created')
-         }}
-       />
-    );
-    assert(content.find('onSubmit'));
-  });
+
 });
